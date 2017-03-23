446119e524503e70fd13b760b6de1920d5872ac5
Marek Rydlewski <marek.rydlewski@outlook.com>
2017-03-24 00:25:30 +0100

    Reading data

--- wumpus.py
+++ wumpus.py
@@ -1,14 +1,19 @@
 import sys
+import numpy as np
 
 
 def wumpus():
     with open(sys.argv[1], "r") as input_f:
-        input = input_f.read()
+        input = input_f.readlines()
+
+    n, m = map(int, input[0].strip().split(' '))
+    prob_hole = float(input[1])
+    data = np.array([list(line.strip()) for line in input[2:]])
+
+    output = np.zeros((n, m), float)
 
     with open(sys.argv[2], "w+") as output_f:
         pass
-
-    print(input)
     return
 
 
